5e7253bd4900b099249b8e087afbb3fd8e83f5a8
travis85904 <travis85904@gmail.com>
2017-03-20 18:47:58 -0700

    getAverage(): got rid of double[] score

--- Assignment-7/gradeBook/GradeBook.java
+++ Assignment-7/gradeBook/GradeBook.java
@@ -39,33 +39,43 @@ class GradeBook {
         scores[nameIndex][scoreIndex] = score;
     }
 
+    //initialize accumulator "avg" to add sum of tests
+    //sort the students test scores from low to high
+
+    //if the student took more than 1 test, drop the students lowest score,
+    //then get the sum of all test scores, and divide by number of tests - 1. (-1 because we dropped their lowest score.)
+    //if student only took 1 test, we will return the score of that test.
+
+    /*Changes:
+     *got rid of redundant local variable score[]
+     *
+     */
     double getAverage(int nameIndex) {
         double avg = 0;
-        double[] score = new double[numTests];
-        System.arraycopy(scores[nameIndex], 0, score, 0, numTests);
-        Arrays.sort(score);
-        if (score.length > 1) {
-            score[0] = 0;
-            for (int i = 1; i < score.length; i++) {
-                avg += score[i];
+        Arrays.sort(scores[nameIndex]);
+        if (scores[nameIndex].length > 1) {
+            scores[nameIndex][0] = 0;
+            for (int i = 1; i < scores[nameIndex].length; i++) {
+                avg += scores[nameIndex][i];
             }
             return avg / (numTests - 1);
-        } else
-            return score[0];
+        } else {
+            return scores[nameIndex][0];
+        }
     }
 
     String getLetterGrade(int index) {
         String grade;
         if (getAverage(index) >= 90) {
-            grade = "A";
+            return "A";
         } else if (getAverage(index) >= 80) {
-            grade = "B";
+            return "B";
         } else if (getAverage(index) >= 70) {
-            grade = "C";
+            return "C";
         } else if (getAverage(index) >= 60) {
-            grade = "D";
-        } else
-            grade = "F";
-        return grade;
+            return "D";
+        } else {
+            return "F";
+        }
     }
 }
